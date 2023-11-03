6ec28849a405c527f09c0e8583a64f78284d5076
Yimin-Jin <yiminjin@microsoft.com>
2023-11-03 15:57:01 +0800

    first commit

---
+++ Interop/TeamsSDK/TeamsInstanceSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace sso_tab_bug_demo.Interop.TeamsSDK;
+
+public class TeamsInstanceSettings
+{
+    public string ContentUrl { get; set; }
+    public string EntityId { get; set; }
+    public string RemoveUrl { get; set; }
+    public string SuggestedDisplayName { get; set; }
+    public string WebsiteUrl { get; set; }
+}
